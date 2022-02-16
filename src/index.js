7cbffca8fb73853ec4cf3d152774e5f256d24c72
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-16 17:03:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,19 @@ function App() {
       <h1>Weather App</h1>
       <Weather />
       <h2>
-        Coded open-sourse by{" "}
-        <a
-          href="https://app.netlify.com/teams/margotmetzemaekers/overview"
-          Margot
-          Metzemaekers
-        ></a>
+        Coded open-source by{" "}
+        <a href="https://app.netlify.com/teams/margotmetzemaekers/overview">
+          Margot Metzemaekers
+        </a>
+        <div>
+          <h2>
+            {" "}
+            Hosted on
+            <a href="https://app.netlify.com/sites/unruffled-hodgkin-e9e42f/overview">
+              Netlify
+            </a>
+          </h2>
+        </div>
       </h2>
     </div>
   );
