867f125329bdfd853c02c48543e924529b3f9dfc
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-16 16:40:28 +0100

    Started React in VS

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
