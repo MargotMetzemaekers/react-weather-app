dbfdef43425514b15cbafae744f30421551bc19b
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-16 16:50:30 +0100

    Removed standard files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import "./App.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
